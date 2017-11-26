97edb2f5916a252d03807a5997f3ae4fe4411b70
Wesley Wigham <wwigham@gmail.com>
2017-11-25 22:43:02 -0800

    FML work

--- get-header/index.js
+++ get-header/index.js
@@ -4,16 +4,22 @@ module.exports = function (context, req) {
 
     if (req.query.text || (req.body && req.body.text)) {
         const text = req.query.text || req.body.text;
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + text
-        };
+        getPNG(text).then(buf => {
+            context.res = {
+                body: buf,
+                headers: {
+                    "Content-Type": "image/png"
+                }
+            };
+            context.done();
+        })
+
     }
     else {
         context.res = {
             status: 400,
             body: "Please pass text on the query string or in the request body"
         };
+        context.done();
     }
-    context.done();
 };
